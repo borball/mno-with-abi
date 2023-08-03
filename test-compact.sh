caa5439fb40cc1b65721637943f05e9d42f9ef14
Borball Zhai <borball.zh@gmail.com>
2023-08-03 17:42:47 -0400

    update test compact

--- test-compact.sh
+++ test-compact.sh
@@ -7,7 +7,7 @@ ssh 192.168.58.14 kcli delete vm compact-master1 compact-master2 compact-master3
 ssh 192.168.58.14 'kcli create vm -P uuid=22222222-1111-1111-0000-000000000000 -P start=False -P memory=20480 -P numcpus=16 -P disks=[122,50] -P nets=["{\"name\":\"br-vlan58\",\"nic\":\"eth0\",\"mac\":\"de:ad:be:ff:10:30\"}"] compact-master0'
 ssh 192.168.58.14 'kcli create vm -P uuid=22222222-1111-1111-0000-000000000001 -P start=False -P memory=20480 -P numcpus=16 -P disks=[122,50] -P nets=["{\"name\":\"br-vlan58\",\"nic\":\"eth0\",\"mac\":\"de:ad:be:ff:10:31\"}"] compact-master1'
 ssh 192.168.58.14 'kcli create vm -P uuid=22222222-1111-1111-0000-000000000002 -P start=False -P memory=20480 -P numcpus=16 -P disks=[122,50] -P nets=["{\"name\":\"br-vlan58\",\"nic\":\"eth0\",\"mac\":\"de:ad:be:ff:10:32\"}"] compact-master2'
-#ssh 192.168.58.14 'kcli create vm -P uuid=22222222-1111-1111-0000-000000000003 -P start=False -P memory=20480 -P numcpus=16 -P disks=[122] -P nets=["{\"name\":\"br-vlan58\",\"nic\":\"eth0\",\"mac\":\"de:ad:be:ff:10:33\"}"] compact-master3'
+#ssh 192.168.58.14 'kcli create vm -P uuid=22222222-1111-1111-0000-000000000003 -P start=False -P memory=20480 -P numcpus=16 -P disks=[122,50] -P nets=["{\"name\":\"br-vlan58\",\"nic\":\"eth0\",\"mac\":\"de:ad:be:ff:10:33\"}"] compact-master3'
 #ssh 192.168.58.14 'kcli create vm -P uuid=22222222-1111-1111-0000-000000000010 -P start=False -P memory=20480 -P numcpus=16 -P disks=[122,50] -P nets=["{\"name\":\"br-vlan58\",\"nic\":\"eth0\",\"mac\":\"de:ad:be:ff:10:40\"}"] compact-worker0'
 
 ssh 192.168.58.14 kcli list vm
