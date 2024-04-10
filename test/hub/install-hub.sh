b14fa1c0ec6bfa6e1e48ffbe0a720242847a9813
Borball Zhai <borball.zh@gmail.com>
2024-04-10 13:32:18 -0400

    add hub

---
+++ test/hub/install-hub.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+cluster="hub"
+version=stable-4.15
+
+basedir="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+root_path="$( cd "$(dirname "$0")/../.." >/dev/null 2>&1 ; pwd -P )"
+iso="$root_path"/mno-iso.sh
+mno_workspace="$root_path"/instances/$cluster
+install="$root_path"/mno-install.sh
+config="$basedir"/cluster-config.yaml
+extra_manifests="$root_path"/extra-manifests
+
+copy_extra_manifests(){
+  cp -r "$basedir"/extra-manifests/. $extra_manifests/
+}
+
+install_ocp(){
+  echo "Install OCP $cluster"
+  rm -rf $mno_workspace
+  $iso $config $version
+  cp $mno_workspace/agent.x86_64.iso /var/www/html/iso/$cluster.iso
+  cp $mno_workspace/auth/kubeconfig /root/workload-enablement/kubeconfigs/kubeconfig-$cluster.yaml
+  $install $config
+}
+
+$basedir/create-kvms.sh
+
+copy_extra_manifests
+install_ocp
