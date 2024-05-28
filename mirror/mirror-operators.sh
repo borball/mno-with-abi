881548620a80d33b75b083dab6ba7fa911f41ee2
Borball Zhai <borball.zh@gmail.com>
2024-05-27 20:16:12 -0400

    fix issue https://github.com/operator-framework/operator-registry/issues/919

--- mirror/mirror-operators.sh
+++ mirror/mirror-operators.sh
@@ -31,6 +31,7 @@ OPERATOR_LIST=(
   "local-storage-operator"
   "sriov-network-operator"
   "kubernetes-nmstate-operator"
+  "ptp-operator"
   "sriov-fec"
   "metallb-operator"
   "cluster-logging")
@@ -68,49 +69,77 @@ fi
 mirrorOperators() {
   local CATALOG_SRC="$1"
   local CATALOG_DIR="${BUILD_DIR}/${CATALOG_SRC}"
+  local DOCKER_CONFIG_DIR="./auth"
   # see https://docs.openshift.com/container-platform/4.12/cli_reference/opm/cli-opm-install.html
   # https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/latest-${OPERATOR_RELEASE}/opm-linux.tar.gz
-  printf  $(tput setaf 2)"%-60s %-10s"$(tput sgr0)"\n" "== Mirroring $CATALOG_SRC" \
+  printf  $(tput setaf 2)"%-60s %-30s"$(tput sgr0)"\n" "== Mirroring $CATALOG_SRC" \
 	  "Destination: ${LOCAL_REGISTRY}/${LOCAL_REPOSITORY}/${CATALOG_SRC}:v${OPERATOR_RELEASE}"
   mkdir -p "${CATALOG_DIR}"
   if [[ ! -f "${CATALOG_DIR}.Dockerfile" ]]; then
-    echo "Generator ${CATALOG_DIR}.Dockerfile"
+    echo "$(date -u) Generator ${CATALOG_DIR}.Dockerfile"
     # DO NOT USE ORIGINAL OPREATOR INDEX unless we are only adding new operators
     opm generate dockerfile ${CATALOG_DIR}
-    [[ $? -ne 0 ]] && exit -1
+    if [[ $? -ne 0 ]] ; then
+      printf  $(tput setaf 3)"%-60s %-30s"$(tput sgr0)"\n" "== Mirroring $CATALOG_SRC" "Failed"
+      exit -1
+    fi
   fi
 
-  if [[ ! -f "${CATALOG_DIR}.json" ]]; then
-    echo "Extract the index.json from registry.redhat.io/${PRODUCT_REPO}/${CATALOG_SRC}:v${OPERATOR_RELEASE}"
-    opm render registry.redhat.io/${PRODUCT_REPO}/${CATALOG_SRC}:v${OPERATOR_RELEASE} -o json > "${CATALOG_DIR}.json"
-    [[ $? -ne 0 ]] && exit -1
+  # Check if we need to render the index again
+  if [[ $(jq '.schema' "${CATALOG_DIR}.json" 2>/dev/null|wc -l) -lt 1 ]]; then
+    echo "$(date -u) Extract the index.json from registry.redhat.io/${PRODUCT_REPO}/${CATALOG_SRC}:v${OPERATOR_RELEASE}"
+    [[ -d "${DOCKER_CONFIG_DIR}" ]] || mkdir -p "${DOCKER_CONFIG_DIR}"
+    cp -u "${LOCAL_SECRET_JSON}" ${DOCKER_CONFIG_DIR}/config.json
+    DOCKER_CONFIG=${DOCKER_CONFIG_DIR} opm render registry.redhat.io/${PRODUCT_REPO}/${CATALOG_SRC}:v${OPERATOR_RELEASE} -o json > "${CATALOG_DIR}.json"
+    if [[ $? -ne 0 ]] ; then
+      printf  $(tput setaf 3)"%-60s %-30s"$(tput sgr0)"\n" "== Mirroring $CATALOG_SRC" "Failed"
+      exit -1
+    fi
   fi
 
-  echo "Filter ${CATALOG_DIR}.json"
+  echo "$(date -u) Filter ${CATALOG_DIR}.json"
   jq -M "$(filter_expression)" "${CATALOG_DIR}.json" > "${CATALOG_DIR}/index.json"
-  [[ $? -ne 0 ]] && exit -1
+  if [[ $? -ne 0 ]] ; then
+    printf  $(tput setaf 3)"%-60s %-30s"$(tput sgr0)"\n" "== Mirroring $CATALOG_SRC" "Failed"
+    exit -1
+  fi
 
-  echo "Validate ${CATALOG_DIR}"
+  echo "$(date -u) Validate ${CATALOG_DIR}"
   opm validate "${CATALOG_DIR}"
-  [[ $? -ne 0 ]] && exit -1
+  if [[ $? -ne 0 ]] ; then
+    printf  $(tput setaf 3)"%-60s %-30s"$(tput sgr0)"\n" "== Mirroring $CATALOG_SRC" "Failed"
+    exit -1
+  fi
 
-  echo "Build container image ${CATALOG_DIR}.Dockerfile"
+  echo "$(date -u) Build container image ${CATALOG_DIR}.Dockerfile"
   ${cmd} build ${BUILD_DIR} -f ${CATALOG_DIR}.Dockerfile \
       -t "${LOCAL_REGISTRY}/${LOCAL_REPOSITORY}/${CATALOG_SRC}:v${OPERATOR_RELEASE}"
-  [[ $? -ne 0 ]] && exit -1
+  if [[ $? -ne 0 ]] ; then
+    printf  $(tput setaf 3)"%-60s %-30s"$(tput sgr0)"\n" "== Mirroring $CATALOG_SRC" "Failed"
+    exit -1
+  fi
 
-  echo "Push container image ${LOCAL_REGISTRY}/${LOCAL_REPOSITORY}/${CATALOG_SRC}:v${OPERATOR_RELEASE}"
+  echo "$(date -u) Push container image ${LOCAL_REGISTRY}/${LOCAL_REPOSITORY}/${CATALOG_SRC}:v${OPERATOR_RELEASE}"
   ${cmd} push ${LOCAL_REGISTRY}/${LOCAL_REPOSITORY}/${CATALOG_SRC}:v${OPERATOR_RELEASE}
-  [[ $? -ne 0 ]] && exit -1
+  if [[ $? -ne 0 ]] ; then
+    printf  $(tput setaf 3)"%-60s %-30s"$(tput sgr0)"\n" "== Mirroring $CATALOG_SRC" "Failed"
+    exit -1
+  fi
 
   # mirror based on the pruned index
-  echo "Mirroring operator images from ${LOCAL_REGISTRY}/${LOCAL_REPOSITORY}/${CATALOG_SRC}:v${OPERATOR_RELEASE}"
+  echo "$(date -u) Mirroring operator images from ${LOCAL_REGISTRY}/${LOCAL_REPOSITORY}/${CATALOG_SRC}:v${OPERATOR_RELEASE}"
   oc adm catalog mirror ${LOCAL_REGISTRY}/${LOCAL_REPOSITORY}/${CATALOG_SRC}:v${OPERATOR_RELEASE} \
     "${LOCAL_REGISTRY}/${LOCAL_REPOSITORY}" \
     --to-manifests="${BUILD_DIR}/manifests/${CATALOG_SRC}" \
     --max-per-registry=2 --request-timeout='1m' \
     -a ${LOCAL_SECRET_JSON} --index-filter-by-os='linux/amd64' \
     --continue-on-error=false --dir="${CATALOG_DIR}"
+  if [[ $? -ne 0 ]] ; then
+    printf  $(tput setaf 3)"%-60s %-30s"$(tput sgr0)"\n" "== Mirroring $CATALOG_SRC" "Failed"
+    exit -1
+  else
+    printf  $(tput setaf 2)"%-60s %-30s"$(tput sgr0)"\n" "== Mirroring $CATALOG_SRC" "Completed"
+  fi
 }
 
 mirrorOperators "redhat-operator-index"
