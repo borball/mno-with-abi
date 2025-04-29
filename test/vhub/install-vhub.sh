ad4e01932dd89728a7cdc4845edff2a331c4045f
Borball Zhai <borball.zh@gmail.com>
2025-04-29 17:01:52 -0400

    add vhub cluster

---
+++ test/vhub/install-vhub.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cluster="vhub"
+version=4.18.10
+
+basedir="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+root_path="$( cd "$(dirname "$0")/../.." >/dev/null 2>&1 ; pwd -P )"
+iso="$root_path"/mno-iso.sh
+mno_workspace="$root_path"/instances/$cluster
+install="$root_path"/mno-install.sh
+config="$basedir"/cluster-config.yaml
+
+install_ocp(){
+  echo "Install OCP $cluster"
+  rm -rf $mno_workspace
+  $iso $config $version
+  cp $mno_workspace/agent.x86_64.iso /var/www/html/iso/$cluster.iso
+  cp $mno_workspace/auth/kubeconfig /root/workload-enablement/kubeconfigs/kubeconfig-$cluster.yaml
+  $install $config
+}
+
+install_ocp
